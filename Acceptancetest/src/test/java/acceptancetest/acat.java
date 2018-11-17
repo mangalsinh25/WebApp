a140ef655d696f699cb305125c0537946c561be7
Mangalsinh <45058216+mangalsinh25@users.noreply.github.com>
2018-11-17 12:36:13 +0530

    Change IP

--- Acceptancetest/src/test/java/acceptancetest/acat.java
+++ Acceptancetest/src/test/java/acceptancetest/acat.java
@@ -12,7 +12,7 @@ public class acat {
 	public HtmlUnitDriver driver;
 	//public WebDriver driver;
 	public WebDriverWait wait;
-	public String URL = "http://54.188.182.180.129:8080/ProdWebapp";
+	public String URL = "http://54.188.182.180:8080/ProdWebapp";
 	public String URL1 = "http://54.188.182.180:8080/ProdWebapp/avncreatepage.jsp";
 	//public String URL = "http://localhost:8080/AVNCommunication-1.0/avnlogin.jsp";
 	//public String URL1 = "http://localhost:8080/AVNCommunication-1.0/avncreatepage.jsp";
